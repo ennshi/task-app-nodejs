35b9d4e143f98af554158fd0c978836347befbd2
ennshi <enn.shi@gmail.com>
2020-01-30 22:35:01 +0100

    Uploading an avatar implemented

--- src/models/user.js
+++ src/models/user.js
@@ -46,7 +46,10 @@ const userSchema = new mongoose.Schema({
             type: String,
             required: true
         }
-    }]
+    }],
+    avatar: {
+        type: Buffer
+    }
 }, {
     timestamps: true
 });
@@ -89,6 +92,7 @@ userSchema.methods.toJSON = function () {
 
     delete userObject.password;
     delete userObject.tokens;
+    delete userObject.avatar;
 
     return userObject;
 };
--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const router = new express.Router();
+const multer = require('multer');
+const sharp = require('sharp');
 
 router.get('/users/me', auth, async (req, res) => {
     res.send(req.user);
@@ -48,6 +50,27 @@ router.post('/users/login', async (req, res) => {
     }
 });
 
+const upload = multer({
+    limits: {
+        fileSize: 2000000
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(jpg|png|jpeg$)/)){
+            return cb(new Error('Please upload right format.'))
+        }
+        cb(undefined, true);
+    }
+});
+
+router.post('/users/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+    req.user.avatar = buffer;
+    await req.user.save();
+    res.send();
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message});
+});
+
 router.post('/users/logout', auth, async (req, res) => {
     try {
         req.user.tokens = req.user.tokens.filter(token => token.token !== req.token);
@@ -91,6 +114,29 @@ router.delete('/users/me', auth, async (req, res) => {
         res.send(req.user);
     } catch(e) {
         res.status(500).send();
+    }
+});
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    try {
+        req.user.avatar = '';
+        await req.user.save();
+        res.send(req.user);
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try{
+        const user = await User.findById(req.params.id);
+        if(!user || !user.avatar) {
+            throw new Error();
+        }
+        res.set('Content-Type', 'image/png');
+        res.send(user.avatar);
+    } catch(e) {
+        res.status(404).send();
     }
 });
 
